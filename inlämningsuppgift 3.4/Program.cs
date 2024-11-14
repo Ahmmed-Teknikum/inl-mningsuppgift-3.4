20b8b1b3405639cc42a6f0bee27638a9a5e09faf
ahka07001 <ahka07001@E-PW05CGQT>
2024-11-14 15:55:57 +0100

    Add project files.

---
+++ inlämningsuppgift 3.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+
+namespace inlämning_._uppgift_._3_._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många minuter är din musik?");
+            int min = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många sekunder är din musik");
+            int sek = int.Parse(Console.ReadLine());
+
+            if ((min <= 2 && sek <= 45) || (min >= 4 && sek >= 20)) 
+            {
+                Console.WriteLine("Din låt får inte spelas");
+            }
+            else
+            {
+                Console.WriteLine("Din låt får spelas");
+            }
+
+            
+            
+            
+                
+            
+
+        }
+    }
+}
